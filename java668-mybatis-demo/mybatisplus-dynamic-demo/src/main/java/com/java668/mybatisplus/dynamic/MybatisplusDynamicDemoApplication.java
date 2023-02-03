306a63331a5351a1e183af4b7f195706e3ea9b81
陈江龙 <chenjianglong@afanticar.com>
2023-02-03 18:18:47 +0800

    mybatis dynamic demo commit

---
+++ java668-mybatis-demo/mybatisplus-dynamic-demo/src/main/java/com/java668/mybatisplus/dynamic/MybatisplusDynamicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.java668.mybatisplus.dynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisplusDynamicDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisplusDynamicDemoApplication.class, args);
+    }
+
+}
---
+++ java668-mybatis-demo/mybatisplus-dynamic-demo/src/test/java/com/java668/mybatisplus/dynamic/MybatisplusDynamicDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java668.mybatisplus.dynamic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisplusDynamicDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
