257d4357e6249127b3a885f50b0582939ad74c05
陈江龙 <zealer_team@163.com>
2023-02-11 15:46:30 +0800

    vue commit

---
+++ java668-springboot-demo/src/main/java/com/java668/springboot/Java668SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.java668.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java668SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java668SpringbootDemoApplication.class, args);
+    }
+
+}
---
+++ java668-springboot-demo/src/test/java/com/java668/springboot/Java668SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java668.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java668SpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
