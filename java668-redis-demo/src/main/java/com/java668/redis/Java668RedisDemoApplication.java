2dba111d1b9a423a2301e66741b9912be1d2e84a
java668 <12401017+java668_real@user.noreply.gitee.com>
2023-02-17 19:11:49 +0800

    redis demo commit

---
+++ java668-redis-demo/src/main/java/com/java668/redis/Java668RedisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.java668.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java668RedisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java668RedisDemoApplication.class, args);
+    }
+
+}
---
+++ java668-redis-demo/src/test/java/com/java668/redis/Java668RedisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java668.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java668RedisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
