aa2e3f4bdf8d9ec6f56354527d24f16d82efa172
java668 <12401017+java668_real@user.noreply.gitee.com>
2023-02-28 14:23:34 +0800

    shiro demo commit

---
+++ java668-shiro-demo/src/main/java/com/java668/shiro/Java668ShiroDemoApplication.java
@@ -0,0 +1,13 @@
+package com.java668.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java668ShiroDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java668ShiroDemoApplication.class, args);
+    }
+
+}
---
+++ java668-shiro-demo/src/test/java/com/java668/shiro/Java668ShiroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java668.shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java668ShiroDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
